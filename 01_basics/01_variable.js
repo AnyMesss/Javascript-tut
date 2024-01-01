fc759d81c39066d2f4af54c72a31c7881725f19c
Animesh Biswas <62756255+AnyMesss@users.noreply.github.com>
2024-01-01 08:44:44 +0000

    made some changes

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -5,15 +5,3 @@ accountCity = "Jaipur"
 
 // accountId = 2 // not allowed
 
-accountEmail = "ab@ab.com"
-accountPassword = "212121"
-accountCity = 'bengaluru'
-let accountState;
-
-/*
-Prefer not to use var
-because of issue in block scope and functional scope
-*/
-
-
-console.table([accounrEmail,accountId,accountPassword,accountCity,accountState]);
