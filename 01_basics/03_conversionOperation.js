28afb75e9e9fc4f5b21998ba8600cfbac66a9437
Animesh Biswas <62756255+AnyMesss@users.noreply.github.com>
2024-01-01 15:55:33 +0000

    made some changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,4 +24,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 69
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ********************* Operations *********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Animesh"
+
+let str3 = str1 + str2
+// console.log(str3); 
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); //32
+
+// console.log(+true);
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++ //If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+// ++gameCounter //If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+console.log(gameCounter);
